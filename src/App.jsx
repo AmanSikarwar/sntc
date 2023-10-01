a8130b2c5991226ca2f432e820aa0434afc58f64
Aman Sikarwar <amansik.1910@gmail.com>
2023-10-02 01:02:23 +0530

    initialised project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Logo from "./components/logo";
+
+function App() {
+  return (
+    <>
+      <Logo />
+      <h3 className="text-center">SNTC IIT Mandi</h3>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/logo.jsx
@@ -0,0 +1,9 @@
+function Logo() {
+  return (
+    <div className="App flex justify-center m-4">
+      <img src="/src/assets/sntc-logo.png" alt="SNTC Logo" width={64} />
+    </div>
+  );
+}
+
+export default Logo;
