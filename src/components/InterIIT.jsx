136db7bbdfc1126b0447b506730e3ec6b4a6cea5
Aman Sikarwar <amansik.1910@gmail.com>
2023-10-05 02:30:44 +0530

    implemented inter-IIT page

--- src/components/InterIIT.jsx
+++ src/components/InterIIT.jsx
@@ -1,5 +1,33 @@
 function InterIIT() {
-  return <div>InterIIT</div>;
+  return (
+    <div className="mx-auto w-full py-4">
+      <div className="flex flex-wrap">
+        <div className="w-full md:w-1/3 md:p-16 hidden sm:block">
+          <img src="/src/assets/tenor.gif" alt="gif" />
+        </div>
+        <div className="w-full md:w-2/3 p-8">
+          <h1 className="text-4xl text-white leading-7 text-center py-4">
+            Inter IIT Tech Meet
+          </h1>
+          <p className="text-lg text-gray-200 py-2 text-center md:text-left">
+            Inter IIT Tech Meet is the annual technological competition
+            organized by the Indian Institutes of Technology and qualifies as
+            the only technical event wherein all the IITs participate. This
+            event is hosted in cycle by one of the participating institutions.
+            At end of each Tech Meet the next host IIT is chosen by mutual
+            consensus.
+          </p>
+          <p className="text-lg text-gray-200 py-2 text-center md:text-left">
+            IIT Mandi is proud to display its achievements and strides displayed
+            by our brilliant contingents.
+          </p>
+          <p className="text-lg text-gray-200 py-2 text-center md:text-left">
+            Stats will be updated soon...
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default InterIIT;
