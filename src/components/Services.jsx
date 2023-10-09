589f91dbeeb7ddd0b0d6c1cae5c397c4ab9400b7
Aman Sikarwar <amansik.1910@gmail.com>
2023-10-09 23:54:24 +0530

    made services page responsive

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,25 +1,36 @@
-import React from 'react'
-
 export default function Services() {
   return (
-    <div className="flex flex-col items-center justify-center mt-[60px]">
-      <div className="text-2xl text-[#0ea5e9]">
+    <div className="lg:m-8 m-2">
+      <div className="text-center text-3xl text-blue-500 font-bold p-4 w-full">
         Services currently hosted on SnTC Server
       </div>
-      <div id='project-container' className="grid grid-cols-3 mt-[30px] justify-between justify-items-center gap-[10px] w-[80%]">
-        <div className="flex flex-wrap py-4 px-4 border-2 text-xl text-center rounded-[5%] w-[48%]">
-           <div className="text-[#0ea5e9] text-center px-5">Judge0 API</div> 
-           <div className='mt-[20px] text-white '>Allows users to compile their programs in a range of language options.</div>
+      <div className="flex flex-wrap justify-center">
+        <div className="flex flex-col py-4 px-4 border-2 text-xl rounded-[5%] m-4 sm:w-1/4 sm:mx-8">
+          <div className="text-blue-500 text-center font-bold justify-around px-5">
+            Judge0 API
+          </div>
+          <div className="mt-2 text-gray-300 text-center ">
+            Allows users to compile their programs in a range of language
+            options.
+          </div>
         </div>
-        <div className="flex flex-wrap py-4 px-4 border-2 w-[48%] text-xl text-center rounded-[5%]">
-            <div className='text-[#0ea5e9] text-center'>Websites for SNTC Clubs</div>
-            <div className='mt-[20px] text-white'>The websites for all clubs under SNTC will be hosted on SNTC Server.</div>
+        <div className="flex flex-col py-4 px-4 border-2 text-xl rounded-[5%] m-4 sm:w-1/4 sm:mx-8">
+          <div className="text-blue-500 text-center font-bold justify-center px-5 sm:w-full">
+            Websites for SNTC Clubs
+          </div>
+          <div className="mt-2 text-gray-300 text-center ">
+            The websites for all clubs under SNTC will be hosted on SNTC Server.
+          </div>
         </div>
-        <div className="flex flex-wrap py-4 px-4 border-2 w-[48%] text-xl text-center rounded-[5%]">
-            <div className='text-[#0ea5e9] text-center'>Student Projects</div>
-            <div className='mt-[20px] text-white'>Student Projects can also be hosted on SNTC Server</div>
+        <div className="flex flex-col py-4 px-4 border-2 text-xl rounded-[5%] m-4 sm:w-1/4 sm:mx-8">
+          <div className="text-blue-500 text-center font-bold justify-center px-5 sm:w-full">
+            Websites for SNTC Clubs
+          </div>
+          <div className="mt-2 text-gray-300 text-center ">
+            The websites for all clubs under SNTC will be hosted on SNTC Server.
+          </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
