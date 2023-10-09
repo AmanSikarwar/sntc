a63cc2322ef214355487ab989fc5a3de6235dede
Aman Sikarwar <amansik.1910@gmail.com>
2023-10-09 19:48:58 +0530

    added InterIIT stats

--- src/components/InterIIT.jsx
+++ src/components/InterIIT.jsx
@@ -21,11 +21,148 @@ function InterIIT() {
             IIT Mandi is proud to display its achievements and strides displayed
             by our brilliant contingents.
           </p>
-          <p className="text-lg text-gray-200 py-2 text-center md:text-left">
-            Stats will be updated soon...
-          </p>
         </div>
       </div>
+      <section className="text-gray-600 body-font">
+        <div className="container px-5 py-4 mx-auto flex flex-wrap">
+          <h2 className="sm:text-3xl text-2xl text-gray-100 font-medium title-font mb-2">
+            🏆 10th Inter-IIT 2022 @ IIT-Kharagpur
+          </h2>
+          <p className="w-full leading-relaxed text-gray-200 py-2">
+            Technical Secretary: <strong>Abhijeet Manhas</strong>
+          </p>
+          <p className="w-full leading-relaxed text-gray-300 py-2">
+            Inter IIT Tech Meet 10.0 Results We have received the following
+            medals this time at Inter IIT Tech Meet 10.0, conducted by IIT
+            Kharagpur.
+          </p>
+          <p className="w-full leading-relaxed text-gray-300 py-2">Medals:</p>
+          <ul className="list-disc list-inside text-gray-300">
+            <li>
+              <strong>
+                🥈 Silver in Blue Yonder&apos;s Next Gen Optimized Delivery
+                system -{" "}
+              </strong>
+              Surendra Singh (b19120) ,Rajat Bansal (b20123),Kanishk Garg
+              (b20109), Aniket Sukhija (b20081), Palak Sharma (b20159)
+            </li>
+            <li>
+              🥉 <strong>Bronze in Silicon Labs SE challenge</strong> - Anurag
+              Maurya (b20183), Shashwat Gupta (b20318),Shalu (b20229), Lalit
+              Narayan Mudgal (b19012), Ankit Karan (b18159), Ishan Dahiya
+              (b18059), Yash Vardhan Sagar
+            </li>
+          </ul>
+          <p className="w-full leading-relaxed text-gray-300 py-2">
+            Also thanks to our Contingent Leader Ankit Karan for managing the
+            teams and submissions so well.
+          </p>
+        </div>
+        <div className="container px-5 py-8 mx-auto flex flex-wrap">
+          <h2 className="sm:text-3xl text-2xl text-gray-100 font-medium title-font mb-2">
+            🏆 9th Inter-IIT 2020 @ IIT-Guwahati
+          </h2>
+          <p className="w-full leading-relaxed text-gray-200 py-2">
+            Technical Secretary: <strong>Vipul Sharma</strong>
+          </p>
+          <p className="w-full leading-relaxed text-gray-300 py-2">
+            Inter IIT Tech Meet 9.0 Results We have received the following
+            medals this time at Inter IIT Tech Meet 9.0, conducted by IIT
+            Guwahati.
+          </p>
+          <p className="w-full leading-relaxed text-gray-300 py-2">Medals:</p>
+          <ul className="list-disc list-inside text-gray-300">
+            <li>
+              <strong>🥇Gold in QuantInsti Algorithmic Trading - </strong>
+              Aryan Goyal, Vineet Ahuja, Dheeraj Yadav, Sanskar Gupta, Anshika
+              Bajpai
+            </li>
+            <li>
+              <strong>🥈Silver in SaptangLabs Security Hackathon - </strong>
+              Milind Topno, Yash Bansod, Pranshu Kharkwal, Dhruv Pindawala,
+              Divyasheel Kumar
+            </li>
+            <li>
+              <strong>
+                🥉Bronze in Bridge i2i Automated Headline and Sentiment
+                Generator -{" "}
+              </strong>
+              Abhijeet Manhas, Khyati Agarwal, Piyush Goyal, Prakhar Uniyal,
+              Naman Tayal, Prajjwal Jha, Manan Shah, Rishabh Garg, Paras Jain,
+              Yash Verma
+            </li>
+          </ul>
+          <p className="w-full leading-relaxed text-gray-300 py-2">
+            Also much thanks to people who participated in{" "}
+            <b>Engineer&apos;s Conclave: </b> Prajwal Sood, Aakash Maurya,
+            Khushi, Manish, Arjun, Vivek Mittal
+          </p>
+          <p className="w-full leading-relaxed text-gray-300 py-1">
+            Also thanks to our Contingent Leader Arjun Sahdev for managing the
+            teams and submissions so well.
+          </p>
+        </div>
+        <div className="container px-5 py-8 mx-auto flex flex-wrap">
+          <h2 className="sm:text-3xl text-2xl text-gray-100 font-medium title-font mb-2">
+            🏆 8th Inter-IIT 2019 @ IIT-Roorkee
+          </h2>
+          <p className="w-full leading-relaxed text-gray-200 py-2">
+            Technical Secretary: <strong>Abhigyan Khaund</strong>
+          </p>
+          <p className="w-full leading-relaxed text-gray-300 py-2">
+            IIT Mandi&apos;s performance at the 8th Inter IIT Tech Meet held at
+            IIT Roorkee from 20-22 December 2019 is really appreciable. This
+            year, for the first time, we participated in all the events and
+            competitions, and our remarkable performance includes four medals
+            which makes it the highest medals earned by us at a Tech Meet.
+          </p>
+          <p className="w-full leading-relaxed text-gray-300 py-2">Medals:</p>
+          <ul className="list-disc list-inside text-gray-300">
+            <li>
+              <strong>🥇Gold in Coding Hackathon - </strong> Aashish Kumar
+              (B16001), Chirag Vashisht (B16094), Milind Topno (B18069), Vipul
+              Sharma (B17069).
+            </li>
+            <li>
+              <strong>🥉Bronze in Case Study - </strong> Param Kashyap (B16098),
+              Rohit Bhamu (B17139), Yuvraj Misra (B19225).
+            </li>
+            <li>
+              <strong>🥉Bronze in BitGrit DataScience Challenge - </strong>
+              Abhijeet Manhas (B18043), Dipanshu Verma (B18054), Nidhi Jain
+              (B18124), Shubhamkumar Pandey(B18194)
+            </li>
+            <li>
+              <strong>🥉Bronze in Route Optimization Algorithm - </strong>
+              Akash Dakoor (B17032), Palak Gupta (B16067), Shreyas Bapat
+              (B16145).
+            </li>
+          </ul>
+          <p className="w-full leading-relaxed text-gray-300 py-2">
+            This year also marked us getting full points for the first time in
+            Engineer&apos;s Conclave and Student Academic Conference
+            (non-competitive).
+          </p>
+          <ul className="list-disc list-inside text-gray-300">
+            <li>
+              <strong>👷 Engineers Conclave - </strong>
+              Five projects, one each by Palak Gupta(B16067), Amit
+              Rajain(B16009) Shreyas Bapat(B160145), Bhavya Bhatt(B16016), Mehul
+              Banga(B19044).
+            </li>
+            <li>
+              <strong>📝 Student Academic Conference - </strong>
+              Five paper presentation, each by Hritik Gupta(B16097), Bhavya
+              Bhatt(B16016), Pratyush Gaurav(B16026), Shreyas Bapat(B160145),
+              Abhigyan Khaund(B16082).
+            </li>
+          </ul>
+          <p className="w-full leading-relaxed text-gray-300 py-1">
+            The performance in all other events were commendable, and lays the
+            plinth for our endeavours in the future.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
